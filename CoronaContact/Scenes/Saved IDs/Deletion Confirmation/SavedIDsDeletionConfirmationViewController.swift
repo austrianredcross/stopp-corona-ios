69fce346810c3223bfb156961b8099ee836b5993
Nils Grabenhorst <nils.grabenhorst@sinnerschrader.com>
2020-05-27 17:06:02 +0200

    CTAA-1397: Adds initial scene for deletion confirmation

---
+++ CoronaContact/Scenes/Saved IDs/Deletion Confirmation/SavedIDsDeletionConfirmationViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SavedIDsDeletionConfirmationViewController.swift
+//  CoronaContact
+//
+
+import UIKit
+import Reusable
+
+final class SavedIDsDeletionConfirmationViewController: UIViewController, StoryboardBased, ViewModelBased, FlashableScrollIndicators {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    var viewModel: SavedIDsDeletionConfirmationViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        flashScrollIndicators()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if navigationController == nil {
+            viewModel?.finish()
+        }
+    }
+
+    private func setupUI() {
+        title = "saved_IDs_deletion_confirmation_title".localized
+    }
+
+    @IBAction func backToStartButtonTapped(_ sender: Any) {
+        viewModel?.deletionConfirmationAcknowledged()
+    }
+}
--- CoronaContact/Scenes/Saved IDs/Saved IDs Info/SavedIDsCoordinator.swift
+++ CoronaContact/Scenes/Saved IDs/Saved IDs Info/SavedIDsCoordinator.swift
@@ -8,8 +8,16 @@ import UIKit
 final class SavedIDsCoordinator: Coordinator {
 
     private lazy var rootViewController: SavedIDsViewController = {
-        SavedIDsViewController.instantiate()
+        let controller = SavedIDsViewController.instantiate()
+        controller.viewModel = SavedIDsViewModel(self)
+        return controller
     }()
+
+    private func confirmationViewController() -> SavedIDsDeletionConfirmationViewController {
+        let controller = SavedIDsDeletionConfirmationViewController.instantiate()
+        controller.viewModel = SavedIDsDeletionConfirmationViewModel(self)
+        return controller
+    }
 
     let navigationController: UINavigationController
 
@@ -18,8 +26,17 @@ final class SavedIDsCoordinator: Coordinator {
     }
 
     override func start() {
-        rootViewController.viewModel = SavedIDsViewModel(self)
         navigationController.pushViewController(rootViewController, animated: true)
+    }
+
+    func didDeleteKeys() {
+        let controller = confirmationViewController()
+        controller.modalPresentationStyle = .fullScreen
+        navigationController.present(controller, animated: true, completion: nil)
+    }
+
+    func deletionConfirmationAcknowledged() {
+
     }
 
     override func finish(animated: Bool = false) {
