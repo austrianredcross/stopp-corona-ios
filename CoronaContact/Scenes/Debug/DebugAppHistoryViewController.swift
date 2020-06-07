2c2f3c7d73fdb5187ff4b8fbbb7c6a93fd573825
Nils Grabenhorst <nils.grabenhorst@sinnerschrader.com>
2020-06-07 20:46:36 +0200

    CTAA-1435: Attempt to pacify SwiftFormat

--- CoronaContact/Scenes/Debug/DebugAppHistoryViewController.swift
+++ CoronaContact/Scenes/Debug/DebugAppHistoryViewController.swift
@@ -4,68 +4,67 @@
 //
 
 #if DEBUG || STAGE
-import UIKit
-import Resolver
+    import Resolver
+    import UIKit
 
-class DebugAppHistoryViewController: UIViewController {
-    
-    struct VersionPresentation {
-        let version: AppVersion
-        var text: String {
-            switch version {
-            case .notPreviouslyInstalled:
-                return "New install (\"What's New\" never shown)"
-            case let version:
-                return version.description
+    class DebugAppHistoryViewController: UIViewController {
+        struct VersionPresentation {
+            let version: AppVersion
+            var text: String {
+                switch version {
+                case .notPreviouslyInstalled:
+                    return "New install (\"What's New\" never shown)"
+                case let version:
+                    return version.description
+                }
             }
         }
-    }
-    
-    @Injected private var whatsNewRepository: WhatsNewRepository
-    
-    private lazy var versions: [VersionPresentation] = {
-        var availableVersions = Set(appVersionHistory.keys)
-        availableVersions.insert(whatsNewRepository.lastWhatsNewShown)
-        availableVersions.insert(.notPreviouslyInstalled)
-        availableVersions.insert("1.2.1")
-        return Array(availableVersions)
-            .sorted()
-            .map(VersionPresentation.init)
-    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "v" + whatsNewRepository.currentAppVersion.description
-    }
-}
+        @Injected private var whatsNewRepository: WhatsNewRepository
 
-extension DebugAppHistoryViewController: UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        versions.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
-        let version = versions[indexPath.row]
-        if version.version == whatsNewRepository.lastWhatsNewShown {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
+        private lazy var versions: [VersionPresentation] = {
+            var availableVersions = Set(appVersionHistory.keys)
+            availableVersions.insert(whatsNewRepository.lastWhatsNewShown)
+            availableVersions.insert(.notPreviouslyInstalled)
+            availableVersions.insert("1.2.1")
+            return Array(availableVersions)
+                .sorted()
+                .map(VersionPresentation.init)
+        }()
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            title = "v" + whatsNewRepository.currentAppVersion.description
         }
-        cell.textLabel?.text = version.text
-        return cell
     }
-}
 
-extension DebugAppHistoryViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selected = versions[indexPath.row]
-        whatsNewRepository.lastWhatsNewShown = selected.version
-        tableView.reloadData()
+    extension DebugAppHistoryViewController: UITableViewDataSource {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            1
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            versions.count
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+            let version = versions[indexPath.row]
+            if version.version == whatsNewRepository.lastWhatsNewShown {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+            cell.textLabel?.text = version.text
+            return cell
+        }
     }
-}
+
+    extension DebugAppHistoryViewController: UITableViewDelegate {
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let selected = versions[indexPath.row]
+            whatsNewRepository.lastWhatsNewShown = selected.version
+            tableView.reloadData()
+        }
+    }
 #endif
