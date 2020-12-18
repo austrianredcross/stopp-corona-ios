8dd74e62805b33ac24a4da405c3ebbeb52ff543e
FRAISS Developer <development@fraiss.com>
2020-12-18 14:33:41 +0100

    Corrected texts on red warning detail screen

--- CoronaContact/Scenes/Shared/Guidelines/AttestedSickness/AttestedSicknessGuidelinesViewModel.swift
+++ CoronaContact/Scenes/Shared/Guidelines/AttestedSickness/AttestedSicknessGuidelinesViewModel.swift
@@ -8,24 +8,33 @@ import Resolver
 
 private let dateFormatter = DateFormatter()
 
-private let dateString: (Date) -> String = { date in
-    dateFormatter.dateFormat = "sickness_certificate_quarantine_guidelines_date_format".localized
+private let quarantineDateString: (Date) -> String = { date in
+    dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "dd.MMMM", options: 0, locale: Locale.current)
     return dateFormatter.string(from: date)
 }
 
 class AttestedSicknessGuidelinesViewModel: ViewModel {
     weak var coordinator: AttestedSicknessGuidelinesCoordinator?
-    @Injected private var localStorage: LocalStorage
-
-    var attestedSicknessAt: String {
-        guard let date = localStorage.attestedSicknessAt else {
-            return ""
-        }
-
-        return dateString(date)
-    }
+    let healthRepository: HealthRepository = Resolver.resolve()
 
     init(with coordinator: AttestedSicknessGuidelinesCoordinator) {
         self.coordinator = coordinator
+    }
+        
+    var endOfQuarantine: String {
+        guard let quarantineDays = healthRepository.userHealthStatus.quarantineDays, let endOfQuarantine = Date().addDays(quarantineDays) else {
+            return ""
+        }
+        
+        return quarantineDateString(endOfQuarantine)
+    }
+    
+    var guidelines: [Instruction] {
+        [
+            .init(index: 1, text: String(format: "sickness_certificate_quarantine_guidelines_steps_first".localized, endOfQuarantine)),
+            .init(index: 2, text: "sickness_certificate_quarantine_guidelines_steps_second".localized),
+            .init(index: 3, text: "sickness_certificate_quarantine_guidelines_steps_third".localized),
+            .init(index: 4, text: "sickness_certificate_quarantine_guidelines_steps_fourth".localized),
+        ]
     }
 }
